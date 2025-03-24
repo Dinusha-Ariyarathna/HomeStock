279d477b211d85715d81380c9e3c14c724941568
dinusha_d <dinushaariyarathna2@gmail.com>
2025-03-24 22:26:56 +0530

    first commit

---
+++ src/test/java/com/example/HomeStock/HomeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
